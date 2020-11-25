05b1f738e5616e14df20bef395adf5b230b2f780
Trevor Gau <trevorsg@gmail.com>
2020-11-24 21:45:41 -0500

    Delete all .js files (favor ts-node)

--- test.js
+++
@@ -1,5 +0,0 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(`import.meta.url: ${import.meta.url}`);
-console.log(`__dirname: ${__dirname}`);
