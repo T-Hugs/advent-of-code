c860ac091bfea2be69738b2c1e0cb019eeebd0c6
Trevor Gau <trevorsg@gmail.com>
2020-11-30 10:58:22 -0500

    Solve 2015 day 23

--- years/2015/23/index.ts
+++ years/2015/23/index.ts
@@ -4,6 +4,7 @@ import * as test from "../../../util/test";
 import chalk from "chalk";
 import * as LOGUTIL from "../../../util/log";
 import { performance } from "perf_hooks";
+import { register } from "ts-node";
 const { log, logSolution, trace } = LOGUTIL;
 
 const YEAR = 2015;
@@ -15,12 +16,76 @@ LOGUTIL.setDebug(DEBUG);
 // data path    : /Users/trevorsg/t-hugs/advent-of-code/years/2015/23/data.txt
 // problem url  : https://adventofcode.com/2015/day/23
 
+function runProgram(program: string[], registers: { [name: string]: number } = {}) {
+	function ensureRegister(name: string) {
+		if (registers[name] == undefined) {
+			registers[name] = 0;
+		}
+	}
+	ensureRegister("pc");
+	while (true) {
+		const instruction = program[registers.pc];
+		if (!instruction) {
+			break;
+		}
+
+		const opcode = instruction.substr(0, 3);
+		const args = instruction.substr(4);
+		let val: number;
+		let reg: string, _val: string;
+		switch (opcode) {
+			case "hlf":
+				ensureRegister(args);
+				registers[args] = Math.floor(registers[args] / 2);
+				registers.pc++;
+				break;
+			case "tpl":
+				ensureRegister(args);
+				registers[args] = registers[args] * 3;
+				registers.pc++;
+				break;
+			case "inc":
+				ensureRegister(args);
+				registers[args]++;
+				registers.pc++;
+				break;
+			case "jmp":
+				val = Number(args);
+				registers.pc += val;
+				break;
+			case "jie":
+				[reg, _val] = args.split(",").map(v => v.trim());
+				ensureRegister(reg);
+				val = Number(_val);
+				if (registers[reg] % 2 === 0) {
+					registers.pc += val;
+				} else {
+					registers.pc++;
+				}
+				break;
+			case "jio":
+				[reg, _val] = args.split(",").map(v => v.trim());
+				ensureRegister(reg);
+				val = Number(_val);
+				if (registers[reg] === 1) {
+					registers.pc += val;
+				} else {
+					registers.pc++;
+				}
+				break;
+		}
+	}
+	return registers;
+}
+
 async function p2015day23_part1(input: string) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	return runProgram(lines)["b"];
 }
 
 async function p2015day23_part2(input: string) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	return runProgram(lines, { a: 1 })["b"];
 }
 
 async function run() {
@@ -28,7 +93,7 @@ async function run() {
 	const part2tests: TestCase[] = [];
 
 	// Run tests
-	test.beginTests()
+	test.beginTests();
 	test.beginSection();
 	for (const testCase of part1tests) {
 		test.logTestResult(testCase, String(await p2015day23_part1(testCase.input)));
@@ -46,10 +111,10 @@ async function run() {
 	const part1Solution = String(await p2015day23_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2015day23_part2(input));
 	const part2After = performance.now();
-	
+
 	logSolution(part1Solution, part2Solution);
 
 	log(chalk.gray("--- Performance ---"));
