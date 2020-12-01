7a4ef7abb8177589b7e4d774574093f78515e51f
Trevor Gau <trevorsg@gmail.com>
2020-11-30 21:53:37 -0500

    Seed 2020. ronpaulitshappening.gif.

---
+++ years/2020/19/index.ts
@@ -0,0 +1,67 @@
+import _ from "lodash";
+import * as util from "../../../util/util";
+import * as test from "../../../util/test";
+import chalk from "chalk";
+import * as LOGUTIL from "../../../util/log";
+import { performance } from "perf_hooks";
+const { log, logSolution, trace } = LOGUTIL;
+
+const YEAR = 2020;
+const DAY = 19;
+const DEBUG = true;
+LOGUTIL.setDebug(DEBUG);
+
+// solution path: /Users/trevorsg/t-hugs/advent-of-code/years/2020/19/index.ts
+// data path    : /Users/trevorsg/t-hugs/advent-of-code/years/2020/19/data.txt
+// problem url  : https://adventofcode.com/2020/day/19
+
+async function p2020day19_part1(input: string) {
+	return "Not implemented";
+}
+
+async function p2020day19_part2(input: string) {
+	return "Not implemented";
+}
+
+async function run() {
+	const part1tests: TestCase[] = [];
+	const part2tests: TestCase[] = [];
+
+	// Run tests
+	test.beginTests()
+	test.beginSection();
+	for (const testCase of part1tests) {
+		test.logTestResult(testCase, String(await p2020day19_part1(testCase.input)));
+	}
+	test.beginSection();
+	for (const testCase of part2tests) {
+		test.logTestResult(testCase, String(await p2020day19_part2(testCase.input)));
+	}
+	test.endTests();
+
+	// Get input and run program while measuring performance
+	const input = await util.getInput(DAY, YEAR);
+
+	const part1Before = performance.now();
+	const part1Solution = String(await p2020day19_part1(input));
+	const part1After = performance.now();
+
+	const part2Before = performance.now()
+	const part2Solution = String(await p2020day19_part2(input));
+	const part2After = performance.now();
+	
+	logSolution(part1Solution, part2Solution);
+
+	log(chalk.gray("--- Performance ---"));
+	log(chalk.gray(`Part 1: ${util.msToString(part1After - part1Before)}`));
+	log(chalk.gray(`Part 2: ${util.msToString(part2After - part2Before)}`));
+	log();
+}
+
+run()
+	.then(() => {
+		process.exit();
+	})
+	.catch(error => {
+		throw error;
+	});
