5a7ea6406f04ab61fea0fe1adcd383bb93cf2b68
Trevor Gau <trevorsg@gmail.com>
2020-12-02 15:39:47 -0500

    Solve 2019 day 1 part 2

--- years/2019/01/index.ts
+++ years/2019/01/index.ts
@@ -1,6 +1,8 @@
 import * as util from "../../../util/util";
+import * as test from "../../../util/test";
 import chalk from "chalk";
 import * as LOGUTIL from "../../../util/log";
+import { performance } from "perf_hooks";
 const { log, logSolution, trace } = LOGUTIL;
 
 const YEAR = 2019;
@@ -13,25 +15,75 @@ LOGUTIL.setDebug(DEBUG);
 // problem url  : https://adventofcode.com/2019/day/1
 
 async function p2019day1_part1(input: string) {
-	return String(
-		input
-			.split("\n")
-			.map(Number)
-			.reduce((p, c) => p + (Math.floor(c / 3) - 2), 0)
-	);
+	return input
+		.split("\n")
+		.map(Number)
+		.reduce((p, c) => p + (Math.floor(c / 3) - 2), 0);
 }
 
-async function p2019day1_part2(input: string, part1Solution: string) {
-	return "Not implemented";
+async function p2019day1_part2(input: string) {
+	return input
+		.split("\n")
+		.map(Number)
+		.reduce((p, c) => {
+			let totalFuel = 0;
+			let extraFuel = c;
+			while (true) {
+				extraFuel = Math.floor(extraFuel / 3) - 2;
+				if (extraFuel <= 0) {
+					return totalFuel + p;
+				}
+				totalFuel += extraFuel;
+			}
+		}, 0);
 }
 
 async function run() {
+	const part1tests: TestCase[] = [];
+	const part2tests: TestCase[] = [
+		{
+			input: `14`,
+			expected: `2`,
+		},
+		{
+			input: `1969`,
+			expected: `966`,
+		},
+		{
+			input: `100756`,
+			expected: `50346`,
+		},
+	];
+
+	// Run tests
+	test.beginTests();
+	test.beginSection();
+	for (const testCase of part1tests) {
+		test.logTestResult(testCase, String(await p2019day1_part1(testCase.input)));
+	}
+	test.beginSection();
+	for (const testCase of part2tests) {
+		test.logTestResult(testCase, String(await p2019day1_part2(testCase.input)));
+	}
+	test.endTests();
+
+	// Get input and run program while measuring performance
 	const input = await util.getInput(DAY, YEAR);
 
+	const part1Before = performance.now();
 	const part1Solution = String(await p2019day1_part1(input));
-	const part2Solution = String(await p2019day1_part2(input, part1Solution));
+	const part1After = performance.now();
+
+	const part2Before = performance.now();
+	const part2Solution = String(await p2019day1_part2(input));
+	const part2After = performance.now();
 
 	logSolution(1, 2019, part1Solution, part2Solution);
+
+	log(chalk.gray("--- Performance ---"));
+	log(chalk.gray(`Part 1: ${util.msToString(part1After - part1Before)}`));
+	log(chalk.gray(`Part 2: ${util.msToString(part2After - part2Before)}`));
+	log();
 }
 
 run()
