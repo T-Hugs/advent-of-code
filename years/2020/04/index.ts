9e8ee9d10dfab65314b9357d468033226c27b84d
Trevor Gau <trevorsg@gmail.com>
2020-12-04 00:44:14 -0500

    Solve 2020 day 4

--- years/2020/04/index.ts
+++ years/2020/04/index.ts
@@ -16,11 +16,76 @@ LOGUTIL.setDebug(DEBUG);
 // problem url  : https://adventofcode.com/2020/day/4
 
 async function p2020day4_part1(input: string) {
-	return "Not implemented";
+	const passports = input.split("\n\n");
+	const req = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+	let count = 0;
+	for (const passport of passports) {
+		const passportData = passport.split("\n").join(" ");
+		const passportVals = passportData.split(" ");
+		const needs = [...req];
+
+		for (const val of passportVals) {
+			const split = val.split(":");
+			_.remove(needs, v => v === split[0]);
+		}
+
+		if (needs.length === 0) {
+			count++;
+		}
+	}
+	return count;
 }
 
 async function p2020day4_part2(input: string) {
-	return "Not implemented";
+	const passports = input.split("\n\n");
+	const validations: { [key: string]: (test: string) => boolean } = {
+		byr: function (test: string) {
+			return /\d{4}/.test(test) && Number(test) >= 1920 && Number(test) <= 2002;
+		},
+		iyr: (test: string) => {
+			return /\d{4}/.test(test) && Number(test) >= 2010 && Number(test) <= 2020;
+		},
+		eyr: (test: string) => {
+			return /\d{4}/.test(test) && Number(test) >= 2020 && Number(test) <= 2030;
+		},
+		hgt: (test: string) => {
+			if (test.endsWith("cm")) {
+				const val = parseInt(test);
+				return val >= 150 && val <= 193;
+			} else if (test.endsWith("in")) {
+				const val = parseInt(test);
+				return val >= 59 && val <= 76;
+			}
+			return false;
+		},
+		hcl: (test: string) => /^#[0-9a-f]{6}$/.test(test),
+		ecl: (test: string) => /^amb|blu|brn|gry|grn|hzl|oth$/.test(test),
+		pid: (test: string) => /^\d{9}$/.test(test),
+		cid: (test: string) => true,
+	};
+	let count = 0;
+	for (const passport of passports) {
+		const passportData = passport.split("\n").join(" ");
+		const passportVals = passportData.split(" ");
+		const parsedPassport: { [key: string]: string } = {};
+
+		for (const val of passportVals) {
+			const split = val.split(":");
+			parsedPassport[split[0]] = split[1];
+		}
+
+		let good = true;
+		for (const validation of Object.entries(validations)) {
+			if (!validation[1](parsedPassport[validation[0]])) {
+				good = false;
+			}
+		}
+
+		if (good) {
+			count++;
+		}
+	}
+	return count;
 }
 
 async function run() {
@@ -28,7 +93,7 @@ async function run() {
 	const part2tests: TestCase[] = [];
 
 	// Run tests
-	test.beginTests()
+	test.beginTests();
 	test.beginSection();
 	for (const testCase of part1tests) {
 		test.logTestResult(testCase, String(await p2020day4_part1(testCase.input)));
@@ -46,10 +111,10 @@ async function run() {
 	const part1Solution = String(await p2020day4_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2020day4_part2(input));
 	const part2After = performance.now();
-	
+
 	logSolution(4, 2020, part1Solution, part2Solution);
 
 	log(chalk.gray("--- Performance ---"));
