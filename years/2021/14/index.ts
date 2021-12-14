ed4c4d84006618f72a07c73174ff301016f748fc
Trevor Gau <trgau@microsoft.com>
2021-12-14 01:26:43 -0500

    Solve 2021 day 14

--- years/2021/14/index.ts
+++ years/2021/14/index.ts
@@ -13,16 +13,122 @@ const DAY = 14;
 // problem url  : https://adventofcode.com/2021/day/14
 
 async function p2021day14_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	let template = lines[0];
+	const rulesS = lines.slice(2);
+
+	const rules: {[key: string]: string} = {}
+	for (const rule of rulesS) {
+		const [from, to] = rule.split(" -> ");
+		rules[from] = to;
+	}
+
+	for (let i = 0; i < 10; ++i) {
+		let newStr = "";
+		for (let i = 0; i < template.length - 1; ++i) {
+			const pair = template[i] + template[i + 1];
+			if (rules[pair]) {
+				newStr += template[i] + rules[pair];// + template[i + 1];
+			} else {
+				newStr += pair;
+			}
+		}
+		newStr += template[template.length - 1];
+		template = newStr;
+	}
+
+	const elems = util.countUniqueElements(template);
+	const max = util.max(Object.entries(elems), elem => elem[1]);
+	const min = util.min(Object.entries(elems), elem => elem[1]);
+	return max.value - min.value;
 }
 
 async function p2021day14_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	let template = lines[0];
+	const rulesS = lines.slice(2);
+
+	const rules: {[key: string]: string} = {}
+	for (const rule of rulesS) {
+		const [from, to] = rule.split(" -> ");
+		rules[from] = to;
+	}
+
+	const counts: Obj<number> = {};
+	let pairCounts: Obj<number> = {};
+
+	for (let i = 0; i < template.length - 1; ++i) {
+		const pair = template[i] + template[i + 1];
+		pairCounts[pair] = (pairCounts[pair] || 0) + 1;
+	}
+
+	for (let i = 0; i < 40; ++i) {
+		const newPairs: Obj<number> = {};
+		for (const pair of Object.entries(pairCounts)) {
+			const middle = rules[pair[0]];
+			const left = pair[0][0] + middle;
+			const right = middle + pair[0][1];
+			newPairs[left] = (newPairs[left] || 0) + pair[1];
+			newPairs[right] = (newPairs[right] || 0) + pair[1];
+		}
+		pairCounts = newPairs;
+	}
+	
+	for (const pair of Object.entries(pairCounts)) {
+		counts[pair[0][1]] = (counts[pair[0][1]] || 0) + pair[1];
+	}
+	counts[template[0]]++;
+
+	const max = util.max(Object.entries(counts), elem => elem[1]);
+	const min = util.min(Object.entries(counts), elem => elem[1]);
+	return (max.value - min.value);
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{
+		input: `NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`,
+		extraArgs: [],
+		expected: `1588`
+	}];
+	const part2tests: TestCase[] = [{
+		input: `NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`,
+		extraArgs: [],
+		expected: `2188189693529`
+	}];
 
 	// Run tests
 	test.beginTests();
