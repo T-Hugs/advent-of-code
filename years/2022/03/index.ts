b5e618847229d4b3e79e0b670c884b0a5a8604b5
Trevor Gau <trgau@microsoft.com>
2022-12-03 15:52:42 -0500

    Solve 2022 Day 3

--- years/2022/03/index.ts
+++ years/2022/03/index.ts
@@ -12,17 +12,72 @@ const DAY = 3;
 // data path    : C:\Users\trgau\dev\t-hugs\advent-of-code\years\2022\03\data.txt
 // problem url  : https://adventofcode.com/2022/day/3
 
+const alpha = "abcdefghijklmnopqrstuvwxyz";
+const priorities = alpha.split("");
+priorities.push(...alpha.toUpperCase().split(""));
+function getPriority(letter: string) {
+	return priorities.indexOf(letter) + 1;
+}
+
 async function p2022day3_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	const commons = [];
+	for (const line of lines) {
+		const first = line.slice(0, line.length / 2);
+		const second = line.slice(line.length / 2);
+		const uniqFirst = util.countUniqueElements(first);
+		for (const l of second) {
+			if (uniqFirst[l]) {
+				commons.push(l);
+				break;
+			}
+		}
+	}
+	return commons.reduce((p, c) => p + getPriority(c), 0);
 }
 
 async function p2022day3_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	const alpha = "abcdefghijklmnopqrstuvwxyz";
+	const badges = [];
+	for (let i = 0; i < lines.length; i += 3) {
+		const first = lines[i];
+		const second = lines[i + 1];
+		const third = lines[i + 2];
+
+		for (const letter of alpha + alpha.toUpperCase()) {
+			if (first.includes(letter) && second.includes(letter) && third.includes(letter)) {
+				badges.push(letter);
+				break;
+			}
+		}
+	}
+	return badges.reduce((p, c) => p + getPriority(c), 0);
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`,
+			extraArgs: [],
+			expected: `157`,
+		},
+	];
+	const part2tests: TestCase[] = [{
+		input: `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`,
+		extraArgs: [],
+		expected: `70`
+	}];
 
 	// Run tests
 	test.beginTests();
@@ -45,7 +100,7 @@ async function run() {
 	const part1Solution = String(await p2022day3_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2022day3_part2(input));
 	const part2After = performance.now();
 
