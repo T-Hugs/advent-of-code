51b03777aef05011d5696dc3646a90e023da23c1
Trevor Gau <trgau@microsoft.com>
2022-12-24 11:33:24 -0500

    Solve 2022 Day 11

--- years/2022/11/index.ts
+++ years/2022/11/index.ts
@@ -12,17 +12,181 @@ const DAY = 11;
 // data path    : C:\Users\trgau\dev\t-hugs\advent-of-code\years\2022\11\data.txt
 // problem url  : https://adventofcode.com/2022/day/11
 
+interface Monkey {
+	items: number[];
+	operator: string;
+	operand: string;
+	divisorTest: number;
+	ifTrue: number;
+	ifFalse: number;
+	inspections: 0;
+}
+
 async function p2022day11_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const NUM_ROUNDS = 20;
+	const monkeys: Monkey[] = [];
+	const groups = input.split("\n\n");
+	for (const group of groups) {
+		const lines = group.split("\n").map(l => l.trim());
+		const startingItems = lines[1].substring("Starting items: ".length).split(", ").map(Number);
+		const [operator, operand] = lines[2].substring("Operation: new = old ".length).split(" ");
+		const divisorTest = Number(lines[3].substring("Test: divisible by ".length));
+		const ifTrue = Number(lines[4].substring("If true: throw to monkey ".length));
+		const ifFalse = Number(lines[5].substring("If false: throw to monkey ".length));
+		monkeys.push({
+			items: startingItems,
+			operator,
+			operand,
+			divisorTest,
+			ifTrue,
+			ifFalse,
+			inspections: 0,
+		});
+	}
+
+	for (let i = 0; i < NUM_ROUNDS; ++i) {
+		for (const monkey of monkeys) {
+			while (true) {
+				const item = monkey.items.shift();
+				if (item) {
+					monkey.inspections++;
+					const operand = monkey.operand === "old" ? item : Number(monkey.operand);
+					const newWorry = Math.floor((monkey.operator === "*" ? item * operand : item + operand) / 3);
+					if (newWorry % monkey.divisorTest === 0) {
+						// log(`Monkey ${monkeys.indexOf(monkey)} threw ${newWorry} to monkey ${monkey.ifTrue}`);
+						monkeys[monkey.ifTrue].items.push(newWorry);
+					} else {
+						// log(`Monkey ${monkeys.indexOf(monkey)} threw ${newWorry} to monkey ${monkey.ifFalse}`);
+						monkeys[monkey.ifFalse].items.push(newWorry);
+					}
+				} else {
+					break;
+				}
+			}
+		}
+	}
+	const inspections = monkeys.map(m => m.inspections).sort((a, b) => b - a);
+	return inspections[0] * inspections[1];
 }
 
 async function p2022day11_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const NUM_ROUNDS = 10000;
+	const monkeys: Monkey[] = [];
+	const groups = input.split("\n\n");
+	for (const group of groups) {
+		const lines = group.split("\n").map(l => l.trim());
+		const startingItems = lines[1].substring("Starting items: ".length).split(", ").map(Number);
+		const [operator, operand] = lines[2].substring("Operation: new = old ".length).split(" ");
+		const divisorTest = Number(lines[3].substring("Test: divisible by ".length));
+		const ifTrue = Number(lines[4].substring("If true: throw to monkey ".length));
+		const ifFalse = Number(lines[5].substring("If false: throw to monkey ".length));
+		monkeys.push({
+			items: startingItems,
+			operator,
+			operand,
+			divisorTest,
+			ifTrue,
+			ifFalse,
+			inspections: 0,
+		});
+	}
+
+	const lcm = Number(util.lcm(monkeys.map(m => BigInt(m.divisorTest))));
+
+	for (let i = 0; i < NUM_ROUNDS; ++i) {
+		for (const monkey of monkeys) {
+			while (true) {
+				const item = monkey.items.shift();
+				if (item) {
+					monkey.inspections++;
+					const operand = monkey.operand === "old" ? item : Number(monkey.operand);
+					const newWorry = Math.floor(monkey.operator === "*" ? item * operand : item + operand) % lcm;
+					if (newWorry % monkey.divisorTest === 0) {
+						// log(`Monkey ${monkeys.indexOf(monkey)} threw ${newWorry} to monkey ${monkey.ifTrue}`);
+						monkeys[monkey.ifTrue].items.push(newWorry);
+					} else {
+						// log(`Monkey ${monkeys.indexOf(monkey)} threw ${newWorry} to monkey ${monkey.ifFalse}`);
+						monkeys[monkey.ifFalse].items.push(newWorry);
+					}
+				} else {
+					break;
+				}
+			}
+		}
+	}
+	const inspections = monkeys.map(m => m.inspections).sort((a, b) => b - a);
+	return inspections[0] * inspections[1];
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `Monkey 0:
+Starting items: 79, 98
+Operation: new = old * 19
+Test: divisible by 23
+	If true: throw to monkey 2
+	If false: throw to monkey 3
+
+Monkey 1:
+Starting items: 54, 65, 75, 74
+Operation: new = old + 6
+Test: divisible by 19
+	If true: throw to monkey 2
+	If false: throw to monkey 0
+
+Monkey 2:
+Starting items: 79, 60, 97
+Operation: new = old * old
+Test: divisible by 13
+	If true: throw to monkey 1
+	If false: throw to monkey 3
+
+Monkey 3:
+Starting items: 74
+Operation: new = old + 3
+Test: divisible by 17
+	If true: throw to monkey 0
+	If false: throw to monkey 1
+`,
+			extraArgs: [],
+			expected: `10605`,
+		},
+	];
+	const part2tests: TestCase[] = [
+		{
+			input: `Monkey 0:
+Starting items: 79, 98
+Operation: new = old * 19
+Test: divisible by 23
+If true: throw to monkey 2
+If false: throw to monkey 3
+
+Monkey 1:
+Starting items: 54, 65, 75, 74
+Operation: new = old + 6
+Test: divisible by 19
+If true: throw to monkey 2
+If false: throw to monkey 0
+
+Monkey 2:
+Starting items: 79, 60, 97
+Operation: new = old * old
+Test: divisible by 13
+If true: throw to monkey 1
+If false: throw to monkey 3
+
+Monkey 3:
+Starting items: 74
+Operation: new = old + 3
+Test: divisible by 17
+If true: throw to monkey 0
+If false: throw to monkey 1
+`,
+			extraArgs: [],
+			expected: `2713310158`,
+		},
+	];
 
 	// Run tests
 	test.beginTests();
@@ -45,7 +209,7 @@ async function run() {
 	const part1Solution = String(await p2022day11_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2022day11_part2(input));
 	const part2After = performance.now();
 
