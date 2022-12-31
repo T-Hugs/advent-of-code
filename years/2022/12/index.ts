a4a1dda9cbd2b18e24ca962017a0e43a950f0ffa
Trevor Gau <trevorsg@gmail.com>
2022-12-30 21:45:35 -0500

    Solve 2022 day 12 and 13

--- years/2022/12/index.ts
+++ years/2022/12/index.ts
@@ -4,6 +4,8 @@ import * as test from "../../../util/test";
 import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
+import { Grid } from "../../../util/grid";
+import aStar from "a-star";
 
 const YEAR = 2022;
 const DAY = 12;
@@ -12,17 +14,79 @@ const DAY = 12;
 // data path    : C:\Users\trgau\dev\t-hugs\advent-of-code\years\2022\12\data.txt
 // problem url  : https://adventofcode.com/2022/day/12
 
+function height(val: string) {
+	if (val === "S") {
+		return 0;
+	}
+	if (val === "E") {
+		return 25;
+	}
+	return val.charCodeAt(0) - "a".charCodeAt(0);
+}
+
 async function p2022day12_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const grid = new Grid({ serialized: input });
+
+	const path = aStar({
+		start: grid.getCell("S")!,
+		isEnd: n => n?.value === "E",
+		neighbor: c => {
+			const neighbors = c.neighbors() ?? [];
+			return neighbors.filter(n => height(n.value) - 1 <= height(c.value));
+		},
+		heuristic: c => 25 - height(c.value),
+		distance: (a, b) => 1,
+	});
+
+	grid.log();
+	return path.path.length - 1;
 }
 
 async function p2022day12_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const grid = new Grid({ serialized: input });
+
+	const lengths: number[] = [];
+	for (const cell of grid.getCells("a")) {
+		const path = aStar({
+			start: cell,
+			isEnd: n => n?.value === "E",
+			neighbor: c => {
+				const neighbors = c.neighbors() ?? [];
+				return neighbors.filter(n => height(n.value) - 1 <= height(c.value));
+			},
+			heuristic: c => 25 - height(c.value),
+			distance: (a, b) => 1,
+		});
+		if (path.status === "success") {
+			lengths.push(path.path.length - 1);
+		}
+	}
+	return Math.min(...lengths);
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi`,
+			extraArgs: [],
+			expected: `31`,
+		},
+	];
+	const part2tests: TestCase[] = [
+		{
+			input: `Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi`,
+			extraArgs: [],
+			expected: `29`,
+		},
+	];
 
 	// Run tests
 	test.beginTests();
@@ -45,7 +109,7 @@ async function run() {
 	const part1Solution = String(await p2022day12_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2022day12_part2(input));
 	const part2After = performance.now();
 
--- years/2022/13/index.ts
+++ years/2022/13/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { first } from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
@@ -8,21 +8,121 @@ import { performance } from "perf_hooks";
 const YEAR = 2022;
 const DAY = 13;
 
+type Packet = Packet[] | number;
+
 // solution path: C:\Users\trgau\dev\t-hugs\advent-of-code\years\2022\13\index.ts
 // data path    : C:\Users\trgau\dev\t-hugs\advent-of-code\years\2022\13\data.txt
 // problem url  : https://adventofcode.com/2022/day/13
 
+function compare(a: Packet, b: Packet) {
+	if (typeof a === "number" && typeof b === "number") {
+		return a - b;
+	}
+	const aCompare: Packet[] = typeof a === "number" ? [a] : a;
+	const bCompare: Packet[] = typeof b === "number" ? [b] : b;
+
+	for (let i = 0; i < aCompare.length; ++i) {
+		const aVal = aCompare[i];
+		const bVal = bCompare[i];
+		if (bVal == undefined) {
+			return 1;
+		}
+		const result = compare(aVal, bVal) as number;
+		if (result !== 0) {
+			return result;
+		}
+	}
+
+	if (aCompare.length === bCompare.length) {
+		return 0;
+	}
+
+	// left ran out of items
+	return -1;
+}
+
 async function p2022day13_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const groups = input.split("\n\n").map(g => g.trim());
+	let index = 1;
+	let sum = 0;
+	for (const group of groups) {
+		if (group.trim()) {
+			const [left, right] = group.split("\n").map(s => JSON.parse(s) as Packet);
+			if (compare(left, right) < 0) {
+				sum += index;
+			}
+			index++;
+		}
+	}
+	return sum;
 }
 
 async function p2022day13_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n").filter(n => n.trim());
+	lines.push("[[2]]", "[[6]]");
+	const sorted = lines.map(l => JSON.parse(l) as Packet).sort(compare);
+	const firstIndex = sorted.findIndex(x => JSON.stringify(x) === "[[2]]") + 1;
+	const secondIndex = sorted.findIndex(x => JSON.stringify(x) === "[[6]]") + 1;
+	return firstIndex * secondIndex;
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`,
+			extraArgs: [],
+			expected: `13`,
+		},
+	];
+	const part2tests: TestCase[] = [{
+		input: `[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`,
+		extraArgs: [],
+		expected: `140`,
+	}];
 
 	// Run tests
 	test.beginTests();
@@ -45,7 +145,7 @@ async function run() {
 	const part1Solution = String(await p2022day13_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2022day13_part2(input));
 	const part2After = performance.now();
 
