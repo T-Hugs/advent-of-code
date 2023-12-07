68816437b70cc48f692c21d9c453f7d6ccb3bf00
Trevor Gau <trevorsg@gmail.com>
2023-12-07 10:21:07 -0500

    Solve 2023 day 7

--- years/2023/07/index.ts
+++ years/2023/07/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { max } from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
@@ -13,16 +13,149 @@ const DAY = 7;
 // data path    : /home/trevorsg/dev/t-hugs/advent-of-code/years/2023/07/data.txt
 // problem url  : https://adventofcode.com/2023/day/7
 
+const cardRanks = Object.fromEntries("AKQJT98765432".split("").map((c, i) => [c, i]));
+const cardRanksJoker = Object.fromEntries("AKQT98765432J".split("").map((c, i) => [c, i]));
+function getCardRank(card: string, useJoker = false) {
+	if (useJoker) {
+		return cardRanksJoker[card];
+	} else {
+		return cardRanks[card];
+	}
+}
+function getHandRank(hand: string, useJoker = false) {
+	if (useJoker) {
+		// ignore jacks when getting unique elements
+		const uniqueElements = util.countUniqueElements(
+			hand
+				.split("")
+				.filter(c => c !== "J")
+				.join("")
+		);
+		let qtys = [...Object.values(uniqueElements)];
+		let cards = [...Object.keys(uniqueElements)];
+		const maxQty = util.max(qtys, undefined, true);
+
+		return getHandRank(
+			hand
+				.split("")
+				.map(c => (c === "J" ? cards[maxQty.index] : c))
+				.join("") || "AAAAA", // in the event we had all jacks
+			false
+		);
+	}
+	const uniqueElements = util.countUniqueElements(hand);
+	let qtys = [...Object.values(uniqueElements)];
+	let cards = [...Object.keys(uniqueElements)];
+
+	if (qtys.length === 1) {
+		if (qtys[0] !== 5) {
+			throw new Error("Should be 5");
+		}
+		return 0;
+	}
+	if (qtys.includes(4)) {
+		if (qtys.length !== 2) {
+			throw new Error("Should be of length 2");
+		}
+		return 1;
+	}
+	if (qtys.length === 2) {
+		if (!(qtys.includes(2) && qtys.includes(3))) {
+			throw new Error("Not a full house?");
+		}
+		return 2;
+	}
+	if (qtys.includes(3)) {
+		return 3;
+	}
+	const twoCount = qtys.filter(x => x === 2);
+	if (twoCount.length === 2) {
+		return 4;
+	}
+	if (twoCount.length === 1) {
+		return 5;
+	}
+	if (qtys.filter(x => x === 1).length === 5) {
+		return 6;
+	}
+	throw new Error("What?");
+}
+
 async function p2023day7_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	const hands: [string, number][] = [];
+	for (const line of lines) {
+		const [hand, bid] = line.split(" ");
+		hands.push([hand, Number(bid)]);
+	}
+
+	hands.sort((a, b) => {
+		const aRank = getHandRank(a[0]);
+		const bRank = getHandRank(b[0]);
+		if (aRank !== bRank) {
+			return aRank - bRank;
+		}
+		for (let i = 0; i < a[0].length; ++i) {
+			if (a[0][i] !== b[0][i]) {
+				return getCardRank(a[0][i]) - getCardRank(b[0][i]);
+			}
+		}
+		return 0;
+	});
+	hands.reverse();
+	let total = 0;
+	for (let i = 0; i < hands.length; ++i) {
+		const points = hands[i][1] * (i + 1);
+		total += points;
+	}
+	return total;
 }
 
 async function p2023day7_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	const hands: [string, number][] = [];
+	for (const line of lines) {
+		const [hand, bid] = line.split(" ");
+		hands.push([hand, Number(bid)]);
+	}
+
+	hands.sort((a, b) => {
+		const aRank = getHandRank(a[0], true);
+		const bRank = getHandRank(b[0], true);
+		if (aRank !== bRank) {
+			return aRank - bRank;
+		}
+		for (let i = 0; i < a[0].length; ++i) {
+			if (a[0][i] !== b[0][i]) {
+				return getCardRank(a[0][i], true) - getCardRank(b[0][i], true);
+			}
+		}
+		return 0;
+	});
+	hands.reverse();
+	const test = hands.map(h => [h, getHandRank(h[0], true)]);
+	console.log(test.length);
+	let total = 0;
+	for (let i = 0; i < hands.length; ++i) {
+		const points = hands[i][1] * (i + 1);
+		total += points;
+	}
+	return total;
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`,
+			extraArgs: [],
+			expected: `6440`,
+			expectedPart2: `5905`,
+		},
+	];
 	const part2tests: TestCase[] = [];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
@@ -48,7 +181,7 @@ async function run() {
 	const part1Solution = String(await p2023day7_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2023day7_part2(input));
 	const part2After = performance.now();
 
