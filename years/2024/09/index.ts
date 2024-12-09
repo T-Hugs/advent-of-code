19b3b8fb66b39b49539b4b7ae80dea49a747e632
Trevor Gau <trevorsg@gmail.com>
2024-12-09 13:57:56 -0500

    Solve 2024 day 9

--- years/2024/09/index.ts
+++ years/2024/09/index.ts
@@ -14,15 +14,105 @@ const DAY = 9;
 // problem url  : https://adventofcode.com/2024/day/9
 
 async function p2024day9_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const files: number[] = [];
+	for (let i = 0; i < input.length; i += 2) {
+		files.push(Number(input[i]));
+	}
+	const freeSpace: number[] = [];
+	for (let i = 1; i < input.length; i += 2) {
+		freeSpace.push(Number(input[i]));
+	}
+
+	const result: number[] = [];
+
+	for (let i = 0; i < files.length; i++) {
+		const file = files[i];
+		for (let j = 0; j < file; ++j) {
+			result.push(i);
+		}
+		const nextFree = freeSpace[i];
+		for (let j = 0; j < nextFree && i !== files.length - 1; ++j) {
+			result.push(files.length - 1);
+			files[files.length - 1]--;
+			while (files[files.length - 1] === 0) {
+				files.pop();
+			}
+		}
+	}
+
+	let checksum = 0;
+	for (let i = 0; i < result.length; i++) {
+		checksum += result[i] * i;
+	}
+	return checksum;
 }
 
 async function p2024day9_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const files: number[] = [];
+	for (let i = 0; i < input.length; i += 2) {
+		files.push(Number(input[i]));
+	}
+	const freeSpace: number[] = [];
+	for (let i = 1; i < input.length; i += 2) {
+		freeSpace.push(Number(input[i]));
+	}
+
+	const result: number[] = [];
+	const movedIndexes: Set<number> = new Set();
+
+	for (let i = 0; i < files.length; ++i) {
+		const file = files[i];
+		if (!movedIndexes.has(i)) {
+			for (let j = 0; j < file; ++j) {
+				result.push(i);
+			}
+		} else {
+			for (let j = 0; j < file; ++j) {
+				result.push(NaN);
+			}
+		}
+
+		// Find the latest file that will fit in this free space
+		for (let j = files.length - 1; j > i; --j) {
+			if (movedIndexes.has(j)) {
+				continue;
+			}
+			if (files[j] <= freeSpace[i]) {
+				for (let k = 0; k < files[j]; ++k) {
+					result.push(j);
+				}
+				movedIndexes.add(j);
+				freeSpace[i] -= files[j];
+			}
+		}
+
+		for (let j = 0; j < freeSpace[i]; ++j) {
+			result.push(NaN);
+		}
+	}
+
+	let checksum = 0;
+	for (let i = 0; i < result.length; i++) {
+		checksum += (result[i] || 0) * i;
+	}
+	return checksum;
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `2333133121414131402`,
+			extraArgs: [],
+			expected: `1928`,
+			expectedPart2: `2858`,
+		},
+		{
+			input: `12345`,
+			extraArgs: [],
+			expected: `60`,
+			expectedPart2: ``,
+		},
+	];
 	const part2tests: TestCase[] = [];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
@@ -48,7 +138,7 @@ async function run() {
 	const part1Solution = String(await p2024day9_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2024day9_part2(input));
 	const part2After = performance.now();
 
